42bfe039d821223d1340dfdb6ce9c4cfbe064707
tadamasahiro <36380718+tadamasahiro@users.noreply.github.com>
2018-04-18 20:33:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def display(frame, decoded_objs):
         frame = cv2.rectangle(frame,
 			      (left, top),
 			      (left + width, height + top),
-			      (0, 255, 0), 2)
+			      (255, 0, 0), 2)
 
     return frame
  
